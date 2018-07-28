2c61278a2b222e9206a1cd4ca7803adafe72b8a3
Shawn Gerrard <shawn@rpatech.me>
2018-07-29 11:10:07 +1200

    added entitlement formulas

--- openfisca_aotearoa/variables/entitlements/callaghan_innovation/getting_started_grant_for_companies.py
+++ openfisca_aotearoa/variables/entitlements/callaghan_innovation/getting_started_grant_for_companies.py
@@ -15,6 +15,15 @@ class getting_started_years_in_business_for_company(Variable):
         reference = "Needs updating"
 	
 
+class getting_started_research_expenditure_for_company(Variable):
+        value_type = float
+        entity = Company
+        default_value = 0
+        definition_period = YEAR
+        label = u'The amount of Research and Development expenditure for the company'
+        reference = "Needs updating"
+
+
 class getting_started_eligible_for_grant_for_company(Variable):
 	value_type = bool
 	entity = Company
@@ -23,7 +32,7 @@ class getting_started_eligible_for_grant_for_company(Variable):
 	reference = "Needs updating"
 
 	def formula(companies, period):
-		return companies("getting_started_years_in_business_for_company", period) == 1       
+		return companies("getting_started_years_in_business_for_company", period) == 1
 
 
 class getting_started_entitlement_for_company(Variable):
@@ -35,5 +44,7 @@ class getting_started_entitlement_for_company(Variable):
 	reference = "Needs updating"
 
 	def formula(company, period, parameters):
-		eligible_company = persons.company("getting_started_eligible_for_grant", period)
-		return eligible_company
+		return select(
+			[getting_started_research_expenditure_for_company <= parameters(period).entitlements.callaghan_innovation.getting_started_grant.getting_started_grant_entitlement_threshold, getting_started_research_expenditure_for_company > parameters(period).entitlements.callaghan_innovation.getting_started_grant.getting_started_grant_entitlement_threshold],
+			[getting_started_research_expenditure_for_company * parameters(period).entitlements.callaghan_innovation.getting_started_grant.getting_started_grant_entitlement_rate, 5000]
+		)
